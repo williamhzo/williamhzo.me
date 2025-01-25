dc7eea096d71a4d9286fd171e34b5467e123ac7d
William H <william.hermozo@protonmail.com>
2025-01-25 13:03:00 +0100

    blog utils

---
+++ lib/blog.ts
@@ -0,0 +1,29 @@
+export type Metadata = {
+  title: string;
+  publishedAt: string;
+  description: string;
+  slug: string;
+  canonicalUrl: string;
+  publish: boolean;
+};
+
+export async function getBlogPosts(): Promise<Metadata[]> {
+  const postsMetadata: Metadata[] = [];
+  for (const post of POSTS) {
+    const { metadata } = await import(`@/content/${post.slug}.mdx`);
+    postsMetadata.push(metadata);
+  }
+  return postsMetadata;
+}
+
+export const POSTS = [
+  {
+    title: 'Letting go of "pixel perfect"',
+    slug: "letting-go-of-pixel-perfect",
+  },
+  {
+    title: 'Unlocking the "blog paralysis"',
+    slug: "unlocking-the-blog-paralysis",
+  },
+  { title: "Writing on the web", slug: "writing-on-the-web" },
+];
