3e373d8665b59cd24c6e4570273436c666f6f66d
William H <william.hermozo@protonmail.com>
2025-01-25 13:03:45 +0100

    show blog index

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import { InlineLink } from "@/components/link";
 import { paths } from "@/constants";
 import { Hero } from "@/components/hero";
 import { SpinningText } from "@/components/spinning-text";
+import Link from "next/link";
+import { getBlogPosts } from "@/lib/blog";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getBlogPosts();
+
   return (
     <div className="mx-auto flex max-w-screen-lg flex-col gap-12 px-6 pt-24 font-mono">
       <div className="mx-auto flex w-full max-w-screen-xl flex-col gap-4">
@@ -21,28 +25,43 @@ export default function Home() {
 
         <div className="group mt-12 flex max-w-prose flex-col gap-0 text-balance">
           <h2>
-            a paris-based <em>product engineer</em> with a keen eye for design,
-            crafting high impact <em>user-focused products</em>.
+            a paris-based <strong>product engineer</strong> with a keen eye for
+            design, crafting high impact <strong>user-focused products</strong>.
           </h2>
 
           <p className="mt-6">
             currently building <InlineLink href={paths.shape}>shape</InlineLink>{" "}
             & <InlineLink href={paths.deca}>deca</InlineLink>, and{" "}
-            <em>having a blast</em>.
+            <strong>having a blast</strong>.
           </p>
         </div>
       </div>
 
       <div className="flex flex-col gap-8">
         <div>
-          <h3 className="mb-4 font-sans font-semibold text-black">writing</h3>
-          <div>letting go of &quot;pixel perfect&quot;</div>
-          <div>unlocking the &quot;blog paralysis&quot;</div>
-          <div>writin on the web</div>
+          <h3 className="text-accent mb-4 font-sans font-semibold">writing</h3>
+
+          <ul className="flex flex-col gap-6">
+            {posts.map((post) => (
+              <li className="" key={post.slug}>
+                <Link
+                  href={paths.notes(post.slug)}
+                  className="group flex flex-col items-start gap-0"
+                >
+                  <span className="group-hover:text-foreground text-accent font-semibold transition-colors duration-150">
+                    {post.title}
+                  </span>
+                  <span className="text-muted-foreground">
+                    {post.description.replace(/\.$/, "")}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
 
         <div>
-          <h3 className="mb-4 font-sans font-semibold text-black">more</h3>
+          <h3 className="text-accent mb-4 font-sans font-semibold">more</h3>
           <p className="max-w-prose text-pretty">
             you can reach me on{" "}
             <InlineLink href={paths.x}>x (twitter)</InlineLink>. my full resume
