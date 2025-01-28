b7524e19cbc1f848e14668e1c642f8849d3d51d1
William H <william.hermozo@protonmail.com>
2025-01-28 19:13:48 +0100

    sans font for body

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,8 +2,6 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    p: ({ children }) => <p className="font-mono">{children}</p>,
-    li: ({ children }) => <li className="font-mono">{children}</li>,
     ...components,
   };
 }
