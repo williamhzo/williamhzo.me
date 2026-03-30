7a9ddbbf8e8a432c1f4ad7189325adf4600c5686
William H <whermozo@gmail.com>
2026-03-30 12:39:00 +0200

    update repo URL to williamhzo.com

--- constants.ts
+++ constants.ts
@@ -5,7 +5,7 @@ export const paths = {
   // external
   x: "https://x.com/williamhzo",
   github: "https://github.com/williamhzo",
-  repo: "https://github.com/williamhzo/williamhzo.me",
+  repo: "https://github.com/williamhzo/williamhzo.com",
   linkedin: "https://www.linkedin.com/in/williamhermozo",
   cv: "https://read.cv/wh",
   deca: "https://deca.art",
